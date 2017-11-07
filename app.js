af0921bb8614177dd7b7636b2af70abaaa79f6eb
RaulVinueza <raulmvinueza@gmail.com>
2017-11-07 10:16:36 -0600

    live test for tweet list

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
 const app = express();
+
+let context = {
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+nunjucks.configure('views',{noCache: true});
+nunjucks.render('index.html', context, (err, output)=>{
+    if (err) throw err;
+    console.log(output);
+});
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+app.use(express.static('public'))
+
+app.use('/', routes);
+
+
 
 app.use((request, response, next)=>{
     console.log(`route: ${request.path}`)
@@ -7,12 +32,13 @@ app.use((request, response, next)=>{
     next();
 })
 
-app.get('/',(request, response)=>{
-    response.status(200).send("Hello world");
-})
-app.get('/news',(request, response)=>{
-    response.status(200).send("<h1>No news</h1>");
-})
+// app.get('/',(request, response)=>{
+//     // response.status(200).send("Hello world");
+//     response.render('index', context);
+// })
+// app.get('/news',(request, response)=>{
+//     response.status(200).send("<h1>No news</h1>");
+// })
 
 
 app.listen(3001,()=>{
